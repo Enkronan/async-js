11fa02ff2bed01f748a8cdbf31e0dadf1e3885ce
enkronan <robin@tempura.se>
2020-05-18 22:14:15 +0200

    type: refactor
    added examples of callbacks

--- callback.js
+++ callback.js
@@ -7,8 +7,27 @@ function loginUser(email, password, callback){
     }, 2000);
 }
 
+function getUserVideos(email, callback){
+    setTimeout(() => {
+        callback (["video1", "video2", "video3"]);
+    }, 1000);
+}
+
+function videoDetails(video, callback){
+    setTimeout(() => {
+        callback ("this is the title");
+    }, 2500);
+}
+
+
 const user = loginUser('test@hotmail.com', 123456, (user) => {
     console.log(user);
+    getUserVideos(user.userEmail, (userVideos) => {
+        console.log(userVideos);
+        videoDetails(userVideos[0], (title) => {
+            console.log(title);
+        });
+    });
 });
 
 console.log("finished");
